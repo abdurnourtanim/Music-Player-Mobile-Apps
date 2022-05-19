09564361f3ea3e13df1f59d8dae2e672f9e3e1c5
antanim <abdurnouutanim11@gmail.com>
2022-05-19 21:35:12 +0600

    App button layer complate

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,24 +1,67 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+
+const { width, height } = Dimensions.get("window");
 
 const MusicPlayer = () => {
   return (
-    <View style={styles.container}>
-      <Ionicons name="heart-outline" size={30} />
-      <Text>React Native MusicPlayer</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      <View style={style.mainContainer}>
+        {/* Image  */}
+
+        {/* Slider */}
+
+        {/* Music controls */}
+      </View>
+
+      <View style={style.bottomSection}>
+        <View style={style.bottomIconWrapper}>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="repeat" size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="share-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="ellipsis-horizontal" size={30} color="#888888" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default MusicPlayer;
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    display: "flex",
-    backgroundColor: "#fff",
+    backgroundColor: "#222831",
+  },
+  mainContainer: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    height: "100%",
+  },
+  bottomSection: {
+    borderTopColor: "#393E46",
+    borderWidth: 1,
+    width: width,
+    alignItems: "center",
+    paddingVertical: 15,
+  },
+  bottomIconWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
   },
 });
